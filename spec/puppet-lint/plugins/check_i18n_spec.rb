e076afd9a2eba6ea58e0dc7137514721e6507017
Eric Putnam <putnam.eric@gmail.com>
2017-08-10 15:11:56 -0700

    (MODULES-5405) add heredoc and interp detection
    This adds detection for here heredocs and interpolation in strings not already decorated. If it finds an undecorated string with interpolation, it skips over it. If it finds an undecorated string with heredoc, it decorates the heredoc tag simliar to the way they're decorated in ruby. We may also want to check for these things in already decorated strings as a warning to say 'hey, you've decorated this but its not going to work because youve got interpolation in there'.

--- spec/puppet-lint/plugins/check_i18n_spec.rb
+++ spec/puppet-lint/plugins/check_i18n_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe 'check_i18n' do
   let(:msg) { "'warning' messages should be decorated: eg translate(message)" }
+  let(:int_msg) { %r{interpolation is not supported} }
+  let(:here_msg) { %r{heredoc detected} }
 
   context 'with fix disabled' do
     context 'function without translate function' do
@@ -13,6 +15,58 @@ describe 'check_i18n' do
 
       it 'creates a warning' do
         expect(problems).to contain_warning(msg).on_line(1).in_column(9)
+      end
+    end
+
+    context 'with ${} interpolation' do
+      let(:code) { 'warning("a message: ${message}")' }
+
+      it 'detects a problem' do
+        expect(problems).to have(1).problems
+      end
+
+      it 'creates warning' do
+        expect(problems[0][:message]).to match(int_msg)
+      end
+
+      it 'expects problem[:interpolation] to be true' do
+        expect(problems[0][:interpolation]).to be_truthy
+      end
+    end
+
+    context 'with $ interpolation' do
+      let(:code) { 'warning("a message: $message")' }
+
+      it 'detects a problem' do
+        expect(problems).to have(1).problems
+      end
+
+      it 'creates warning' do
+        expect(problems[0][:message]).to match(int_msg)
+      end
+
+      it 'expects problem[:interpolation] to be true' do
+        expect(problems[0][:interpolation]).to be_truthy
+      end
+    end
+
+    context 'with a heredoc' do
+      let(:code) do
+        'warning(@(EOL)
+       this is a heredoc
+       | EOL)'
+      end
+
+      it 'detects something' do
+        expect(problems).to have(1).problems
+      end
+
+      it 'creates warning' do
+        expect(problems[0][:message]).to match(here_msg)
+      end
+
+      it 'expects problem[:heredoc] to be true' do
+        expect(problems[0][:heredoc]).to be_truthy
       end
     end
 
@@ -47,6 +101,34 @@ describe 'check_i18n' do
 
       it 'adds a newline to the end of the manifest' do
         expect(manifest).to eq("warning(translate('message'))")
+      end
+    end
+
+    context 'function without a translate function with interpolation' do
+      let(:code) { 'warning("a message: ${message}")' }
+
+      it 'onlies detect a single problem' do
+        expect(problems).to have(1).problem
+      end
+
+      it 'adds a newline to the end of the manifest' do
+        expect(manifest).not_to eq("warning(translate('message'))")
+      end
+    end
+
+    context 'function without a translate function with a heredoc' do
+      let(:code) do
+        'warning(@(EOL)
+       heredoc
+       | EOL)'
+      end
+
+      it 'onlies detect a single problem' do
+        expect(problems).to have(1).problem
+      end
+
+      it 'decorates the heredoc appropriately' do
+        expect(manifest).to match(%r{warning\(translate\(@\(EOL\)\)})
       end
     end
 
